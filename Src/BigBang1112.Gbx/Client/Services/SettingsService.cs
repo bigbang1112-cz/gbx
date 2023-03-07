583dee8c7025cdb6bc1c92244f3b008feeb7f6a7
Petr Pivoňka <petrpiv1@gmail.com>
2023-03-07 17:51:37 +0100

    Add manual parameters

--- Src/BigBang1112.Gbx/Client/Services/SettingsService.cs
+++ Src/BigBang1112.Gbx/Client/Services/SettingsService.cs
@@ -4,4 +4,5 @@ public class SettingsService
 {
     public int MaxFileCountToUpload { get; set; } = int.MaxValue;
     public int MaxFileSizeToUpload { get; set; } = int.MaxValue;
+    public bool AcceptCustomDownloads { get; set; }
 }
