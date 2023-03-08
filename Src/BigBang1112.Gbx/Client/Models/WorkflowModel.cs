74b0723f63c63e48fc0ac48fadae3a414676bdb3
Petr Pivoňka <petrpiv1@gmail.com>
2023-03-08 17:37:53 +0100

    Add workflow funtions

--- Src/BigBang1112.Gbx/Client/Models/WorkflowModel.cs
+++ Src/BigBang1112.Gbx/Client/Models/WorkflowModel.cs
@@ -5,6 +5,8 @@ public class WorkflowModel
     public required string Name { get; set; }
     public required string Route { get; set; }
     public string? Description { get; set; }
+    public bool Beta { get; set; }
+    public string? Version { get; set; }
     public string[] Input { get; set; } = Array.Empty<string>();
     public WorkflowTaskModel[] Tasks { get; set; } = Array.Empty<WorkflowTaskModel>();
 }
---
+++ Src/BigBang1112.Gbx/Client/WorkflowFunctions.cs
@@ -0,0 +1,22 @@
+﻿using GBX.NET.Engines.Game;
+using GBX.NET.Exceptions;
+
+namespace BigBang1112.Gbx.Client;
+
+public static class WorkflowFunctions
+{
+    public static IEnumerable<CGameCtnGhost> GetGhostsFromReplay(CGameCtnReplayRecord replay)
+    {
+        return replay.GetGhosts();
+    }
+
+    public static CGameCtnChallenge GetMapFromReplay(CGameCtnReplayRecord replay)
+    {
+        return replay.Challenge ?? throw new HeaderOnlyParseLimitationException();
+    }
+
+    public static byte[] GetMapDataFromReplay(CGameCtnReplayRecord replay)
+    {
+        return replay.ChallengeData ?? throw new HeaderOnlyParseLimitationException();
+    }
+}
